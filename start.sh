08640e714e3690c18225a3467b2a27ec18abc87b
Nathan Cooper <nacooper01@email.wm.edu>
2019-09-13 13:34:09 -0400

    Add ability to start Docker container and organization structure with prototyping nbs

---
+++ start.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+TAG=tf-2.0rc0
+
+if [ $# -eq 1 ]; then
+	if [ "$1" = "--build" ]; then
+		# Build the docker container
+		docker build -t $TAG .
+	fi
+fi
+
+
+# Run the docker container
+docker run -d --runtime=nvidia -v $(pwd)/main:/tf/main      \
+	-v /mnt/data/sme_qa/models:/tf/data/models     \
+	-v /mnt/data/sme_qa/datasets:/tf/data/datasets \
+	-p 0.0.0.0:6007:6006 -p 8080:8888 $TAG
