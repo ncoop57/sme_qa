c865753519147347c21f081e59bf84cc55cf80b7
ncoop57 <nacooper01@email.wm.edu>
2019-10-08 16:51:07 -0400

    Add eda code for analyzing statistics of data. Also added hugging face to project

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-TAG=tf-2.0rc0
+TAG=sme_qa
 
 if [ $# -eq 1 ]; then
 	if [ "$1" = "--build" ]; then
@@ -14,4 +14,4 @@ fi
 docker run -d --runtime=nvidia -v $(pwd)/main:/tf/main      \
 	-v /mnt/data/sme_qa/models:/tf/data/models     \
 	-v /mnt/data/sme_qa/datasets:/tf/data/datasets \
-	-p 0.0.0.0:6007:6006 -p 8080:8888 $TAG
+	-p 0.0.0.0:6007:6006 -p 8001:8888 $TAG
